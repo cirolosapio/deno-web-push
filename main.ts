41393f54d38a7aa39d9c3bf076023e0f4a50f036
cirolosapio <lscorionjs@gmail.com>
2024-06-12 00:04:26 +0200

    feat: Add endpoint to update user device information
    This commit adds a new endpoint to update the device information for a specific user. The endpoint is a POST request to '/user/:user' and expects the user parameter in the URL and the device information in the request body. The user's device information is stored in the key-value store using the user's ID as the key.
    
    The commit also includes error handling using the 'atomic' method from the key-value store to ensure consistency when updating the device information.
    
    This change improves the functionality of the application by allowing users to update their device information easily.

--- main.ts
+++ main.ts
@@ -18,11 +18,23 @@ const kv = await Deno.openKv()
 
 const router = new Router();
 router
+  .post('/user/:user', async (ctx) => {
+    const { user } = ctx.params
+    const { device } = await ctx.request.body.json()
+    console.log({ user, device })
+    const key = ['users', user, 'device']
+    const userDevice = await kv.get(key)
+    await kv.atomic()
+      .check(userDevice)
+      .set(key, device)
+      .commit()
+    ctx.response.body = { user, device }
+  })
   .post('/subscription/:user', async (ctx) => {
     const { user } = ctx.params
     const { subscription } = await ctx.request.body.json()
     console.log({ user, subscription })
-    const key = ['users', user]
+    const key = ['users', user, 'subscription']
     const userSubscription = await kv.get(key)
     await kv.atomic()
       .check(userSubscription)
