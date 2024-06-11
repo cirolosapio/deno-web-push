25f04c948dd031c54685050894fff05a8e96580b
cirolosapio <lscorionjs@gmail.com>
2024-06-12 00:27:37 +0200

    chore: Update main.ts to enable CORS credentials

--- main.ts
+++ main.ts
@@ -59,7 +59,7 @@ router
   })
 
 const app = new Application();
-app.use(oakCors({ origin: /cirolosapio\.it$/ }))
+app.use(oakCors({ origin: /cirolosapio\.it$/, credentials: true }))
 app.use(router.routes());
 app.use(router.allowedMethods())
 
